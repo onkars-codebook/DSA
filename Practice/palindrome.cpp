fd4c1a0df9169b64643db60e0609fcddc0de0dc0
onkarsathe <113273391+Onkarsathe007@users.noreply.github.com>
2024-10-30 11:01:58 +0530

    Created Practice

---
+++ Practice/palindrome.cpp
@@ -0,0 +1,19 @@
+// Question : Write C++ program with functions a) To print original string followed by reversed string using  stack
+#include<iostream>
+using namespace std;
+class stack 
+{
+    int top;
+    string str;
+    static const int size = 10;
+    char str[size];
+    stack()
+    {
+        top = -1;
+    }
+
+};
+int main()
+{
+
+}
