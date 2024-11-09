892c4af8bdc79c4d9bdc94ef1029298232a0fe03
onkarsathe <113273391+Onkarsathe007@users.noreply.github.com>
2024-11-09 17:05:08 +0530

    Move All Zeros to the End

---
+++ Practice/moveZerotoEnd.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void moveZerosToEnd(int arr[], int n) {
+    int j = 0; // j is the index for the next non-zero element
+
+    // Traverse the array
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    // Fill remaining elements with zero
+    while (j < n) {
+        arr[j] = 0;
+        j++;
+    }
+}
+
+int main() {
+    int arr[] = {0, 1, 9, 0, 3, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    moveZerosToEnd(arr, n);
+
+    cout << "Array after moving zeros to the end: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
