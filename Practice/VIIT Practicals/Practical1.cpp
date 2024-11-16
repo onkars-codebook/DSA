1bede5c777d37a8f055efa0bd949ab7b9a8bc20e
onkarsathe <113273391+Onkarsathe007@users.noreply.github.com>
2024-11-16 23:49:46 +0530

    Stack-Struct

--- Practice/VIIT Practicals/Practical1.cpp
+++ Practice/VIIT Practicals/Practical1.cpp
@@ -1,1 +1,20 @@
 // Problem Statement : 1	Write a Program to Implement stack operations using array 
+#include<iostream>
+using namespace std;
+class Stack {
+    int top;
+    static const int size=10;
+    int STACK[size];
+    Stack()
+    {
+        top = -1;
+    }
+  
+    
+
+};
+int main()
+{
+    cout<<"Hello !";
+    return 0;
+}
