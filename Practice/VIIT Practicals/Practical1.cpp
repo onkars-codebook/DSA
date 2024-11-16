113733194326ac364ef84a9cbacc0befe2870727
onkarsathe <113273391+Onkarsathe007@users.noreply.github.com>
2024-11-16 23:59:33 +0530

    fun-def

--- Practice/VIIT Practicals/Practical1.cpp
+++ Practice/VIIT Practicals/Practical1.cpp
@@ -9,12 +9,40 @@ class Stack {
     {
         top = -1;
     }
-  
-    
-
+    void push(){
+        if(top == size-1){
+            cout<<"Stack Overflow !";
+        }
+        else{
+        int ele;
+        cout<<"Enter a element to be insert :";
+        cin>>ele;
+        top++;
+        STACK[top] = ele;
+        cout<<ele+"- Inserted at index : "+top;
+        }
+    }
+    void pop()
+    {
+        if(top==-1)
+        {
+            cout<<"Array Underflow !";
+        }
+        else{
+        int temp;
+        temp = STACK[top];
+        top--;
+        cout<<temp<<"Popped out !";
+        }
+    }
+    void display(){
+        for(int i=0;i<= top;i++){
+            cout<<STACK[i];
+        }
+    }
+        
 };
 int main()
 {
-    cout<<"Hello !";
-    return 0;
+   
 }
