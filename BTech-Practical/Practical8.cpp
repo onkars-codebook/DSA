30c26a3b153e3121569cacd65c3102d66b1cf282
onkarsathe007 <onkarsathe96k@gmail.com>
2024-11-21 15:37:29 +0530

    insert-in-BST

--- BTech-Practical/Practical8.cpp
+++ BTech-Practical/Practical8.cpp
@@ -1,11 +1,39 @@
 #include<iostream>
 using namespace std;
 class Node{
+    public :
     int data;
     Node* left;
     Node* right;
+    Node(int ele)
+    {
+        data = ele;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
+Node* insert(Node* root,int marks )
+{
+    if(root == nullptr)
+    {
+        return new Node(marks);
+    }
+
+
+    if(marks < root->data)
+    {
+        root->left = insert(root->left,marks);
+    }
+    else{
+        root->right = insert(root->right,marks);
+    }
+}
+void createNode()
+{
+    int ele;
+
+}
 
 int main()
 {
