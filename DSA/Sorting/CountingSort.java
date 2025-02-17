588f57fd68e7ba9158d07eaacd49f01b87c63867
onkarsathe007 <onkarsathe96k@gmail.com>
2025-02-17 18:18:15 +0000

    couting sort

--- DSA/Sorting/CountingSort.java
+++ DSA/Sorting/CountingSort.java
@@ -1,19 +1,22 @@
 public class CountingSort {
-    //Tip : Dry run the code to better understand 
-    // Method is suitable when there are less number of elements.
+    // Tip : Dry run the code to better understand
+    // Method is suitable when there are fewer distinct elements.
     public static void countingSort(int[] arr) {
 
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             max = Math.max(max, arr[i]); // find : Maximum Number
         }
+
         int count[] = new int[max + 1];
 
-        for (int i = 0; i < count.length; i++) {
-            count[arr[i]]++; // calculate : frequencies of each ele
+        // Calculate frequencies of each element
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
         }
 
         int j = 0;
+        // Sorting the elements
         for (int i = 0; i < count.length; i++) {
             while (count[i] > 0) {
                 arr[j] = i;
@@ -21,10 +24,16 @@ public class CountingSort {
                 count[i]--;
             }
         }
+
+        // Print sorted array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 
     public static void main(String[] args) {
-        // Used when minimum number's are there
-
+        // Used when the range of elements is small
+        int[] arr = { 3, 4, 5, 3, 2, 7 };
+        countingSort(arr);
     }
 }
